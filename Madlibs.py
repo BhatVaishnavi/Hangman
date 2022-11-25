3f50c818a76a33574d2c995f12931ae23010cd86
Vaishnavi Bhat <vaishnavibhat07@gmail.com>
2022-11-25 15:35:27 +0530

    Delete Madlibs.py

--- Madlibs.py
+++
@@ -1,6 +0,0 @@
-adjective = input("Adjective:")
-verb1 = input("verb1:")
-verb2 = input("verb2:")
-famous_person = input("famous person:")
-madlib = (f"Computer programming is so {adjective}! It makes me so excited all the time because I love to {verb1}! . Stay hydrated and {verb2} like you are {famous_person}.")
-print(madlib)
